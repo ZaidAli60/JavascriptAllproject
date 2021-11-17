08635327bc851d89fc6bd6c7d7df0ddb02f9b350
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-11-17 10:01:43 -0800

    complete task

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const body = document.querySelector("body");
 const colors = ["red","blue","green","pink","yellow"];
 
 body.style.backgroundColor= "violet";
+button.addEventListener("click", changeBackgroundColor);
+
+
+function changeBackgroundColor(){
+    const colorIndex =parseInt(Math.random()*colors.length)
+    body.style.backgroundColor = colors[colorIndex];
+}
