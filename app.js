c0b847af541dc07524f9444d230191f6a52b3bc7
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-11-17 09:49:53 -0800

    add colors

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 //Choose a random color
+
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const colors = ["red","blue","green","pink","yellow"];
+
+body.style.backgroundColor= "violet";
